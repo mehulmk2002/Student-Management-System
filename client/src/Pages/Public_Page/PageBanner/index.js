a52bda5fb7d9c1e17af9086beaa3991e3d1ddd18
Mehul Kurkute <100382362+mehulmk2002@users.noreply.github.com>
2023-10-11 18:13:28 +0530

    Update index.js

--- client/src/Pages/Public_Page/PageBanner/index.js
+++ client/src/Pages/Public_Page/PageBanner/index.js
@@ -6,16 +6,16 @@ import './style.css'
 const PageBanner = ({rout}) => {
   return (
     <div>
-         <section id="page-banner" class="pt-105 pb-110 bg_cover" style={{ backgroundImage: `url(${backbanner})`}} data-overlay="8" >
-          <div class="countainer-sec">
-            <div class="row">
-              <div class="col-lg-12">
-                <div class="page-banner-cont">
+         <section id="page-banner" className="pt-105 pb-110 bg_cover" style={{ backgroundImage: `url(${backbanner})`}} data-overlay="8" >
+          <div className="countainer-sec">
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="page-banner-cont">
                   <h2>{rout}</h2>
                   <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb">
-                      <li class="breadcrumb-item"><Link to="/">Home</Link></li>
-                      <li class="breadcrumb-item active">{rout}</li>
+                    <ol className="breadcrumb">
+                      <li className="breadcrumb-item"><Link to="/">Home</Link></li>
+                      <li className="breadcrumb-item active">{rout}</li>
                     </ol>
                   </nav>
                 </div>
